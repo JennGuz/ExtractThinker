5ed0135df6423697d41e5f0dea8a886687c3ab9a
Jenniffer Guzman <jenniffer.guzman122@gmail.com>
2025-07-04 10:47:28 -0500

    se extraen más datos de la factura

--- examples/extractor_basic.py
+++ examples/extractor_basic.py
@@ -1,27 +1,30 @@
 import os
 
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
-from extract_thinker import DocumentLoaderTesseract, Extractor, Contract
+from extract_thinker import DocumentLoaderTesseract, Extractor
 
 load_dotenv()
 cwd = os.getcwd()
 
+class InvoiceContract(BaseModel):
+    invoice_number: str = Field(..., description="Número de la factura")
+    invoice_date: str = Field(..., description="Fecha de emisión de la factura")
+    destinatary: str = Field(..., description="Destinatario de la factura")
+    summary: str = Field(..., description="Resumen de la factura con al menos de 200 caracteres")
 
-class InvoiceContract(Contract):
-    invoice_number: str
-    invoice_date: str
 
+tesseract_path = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-# Use the direct path to tesseract
-tesseract_path = "/opt/homebrew/bin/tesseract"
 test_file_path = os.path.join(cwd, "tests", "test_images", "invoice.png")
 
+print("Iniciando programa")
 extractor = Extractor()
 extractor.load_document_loader(
     DocumentLoaderTesseract(tesseract_path)
 )
-extractor.load_llm("gpt-4o")
+extractor.load_llm("gpt-4.1-mini")
 
 result = extractor.extract(test_file_path, InvoiceContract)
 
@@ -32,3 +35,5 @@ else:
 
 print("Invoice Number: ", result.invoice_number)
 print("Invoice Date: ", result.invoice_date)
+print("Invoice Destinatary", result.destinatary)
+print("Summary", result.summary)
